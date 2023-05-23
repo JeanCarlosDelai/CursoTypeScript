a5a730993499289f3bcb1ead746203caf08bd21e
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-05-23 13:47:22 -0300

    Validators

--- understanding-type/src/module7.ts
+++ understanding-type/src/module7.ts
@@ -13,9 +13,9 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
   }, 2000);
 });
 
-promise.then(data => {
-    data.split(' ');
-})
+promise.then((data) => {
+  data.split(" ");
+});
 
 // Generic function
 
@@ -30,26 +30,95 @@ promise.then(data => {
 
 // Adicionando estas restriçoes, obrigaotiamente temos que adicionar dois objetos para não dar erro
 function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
-  }
-  
-  const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-  console.log(mergedObj);
+  return Object.assign(objA, objB);
+}
 
-// Another bGeneric function
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
 
+// Another Generic function
 interface Lengthy {
   length: number;
 }
 
 function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = 'Got no value.';
+  let descriptionText = "Got no value.";
   if (element.length === 1) {
-    descriptionText = 'Got 1 element';
+    descriptionText = "Got 1 element";
   } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.';
+    descriptionText = "Got " + element.length + " elements.";
   }
   return [element, descriptionText];
 }
 
-console.log(countAndDescribe(['a']));
+console.log(countAndDescribe(["a"]));
+
+//"keyof" Constraint
+// Primeiro parametro tem que ser um onjeto, e o segundo parametro tem que ser uma chave do objeto
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+// A chave sempre tem que referenciar algo do objeto
+extractAndConvert({ name: "max" }, "name");
+
+// Generic Class
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Manu");
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const masObj = {name: 'Max'};
+// objStorage.addItem(masObj);
+// objStorage.addItem({name: 'Manu'});
+// objStorage.removeItem(masObj);
+// console.log(objStorage.getItems());
+
+// Generic Utility Types
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date):
+CourseGoal {
+  //Partial define que este objeto será do CouseGoal
+  let CourseGoal: Partial<CourseGoal> = {};
+  CourseGoal.title = title;
+  CourseGoal.description = description;
+  CourseGoal.completeUntil = date;
+  return CourseGoal as CourseGoal;
+
+};
+
+//Como é readonly não conseguimos usar métodos de adição e remoção
+const namess: Readonly<string[]> = ['Max', 'Sports'];
+// namess.push('Manu');
+// namess.pop();
+
+
